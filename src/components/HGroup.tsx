ac0c7d05f2f534861db3cea8633db23345659147
hydroperfox <hydroperfox@gmail.com>
2025-01-26 15:24:38 -0300

    Update

--- src/components/HGroup.tsx
+++ src/components/HGroup.tsx
@@ -1,7 +1,23 @@
+import { Alignment } from "../layout/Alignment";
+
 export type HGroupOptions = {
-    
+    gap?: number,
+    /**
+     * Horizontal alignment.
+     */
+    horizontalAlign?: Alignment,
+    /**
+     * Vertical alignment.
+     */
+    verticalAlign?: Alignment,
+    /**
+     * Grow factor of the component relative to
+     * its container. This indicates how much space
+     * the component takes out of a number of items.
+     */
+    grow?: number,
 };
 
-export function HGroup({}:HGroupOptions)
+export function HGroup(options: HGroupOptions)
 {
 }
