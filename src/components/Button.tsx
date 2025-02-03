87d2b3df3c14467b380d1dad4baff1380b94a17f
hydroperfox <hydroperfox@gmail.com>
2025-02-03 06:14:49 -0300

    Update

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,16 @@
 import { pointsToRem } from "../utils/points";
 import extend from "extend";
 import { css } from "@emotion/css";
+import { useContext } from "react";
+import { ThemeContext } from "../theme";
+import Color from "color";
 
 export type ButtonVariant =
     "primary" |
     "secondary" |
     "danger" |
-    "white-outline" |
-    "white-outline-primary";
+    "outline" |
+    "outline-primary";
 
 export type ButtonType =
     "button" | "reset" | "submit";
@@ -35,6 +38,8 @@ export type ButtonOptions =
 
 export function Button(options: ButtonOptions)
 {
+    const theme = useContext(ThemeContext);
+
     const newStyle: React.CSSProperties = {};
 
     if (options.minWidth !== undefined)
@@ -65,6 +70,8 @@ export function Button(options: ButtonOptions)
     }
 
     let className: string = "";
+    
+    const padding = "0.7rem 1rem";
 
     switch (options.variant ?? "secondary")
     {
@@ -72,30 +79,136 @@ export function Button(options: ButtonOptions)
         {
             // uses providedTheme.colors.foreground as character color
             className = css `
+                background: ${theme.colors.secondaryBackground};
+                color: ${theme.colors.foreground};
+                padding: ${padding};
+                border: none;
+                border-radius: none;
+
+                &:hover {
+                    background: ${theme.colors.hoveredSecondaryBackground};
+                }
+
+                &:active {
+                    background: ${theme.colors.pressedBackground};
+                    color: ${theme.colors.pressedForeground};
+                }
+
+                &:focus {
+                    outline: 1px dashed ${theme.colors.focusDashes};
+                    outline-offset: -0.7rem;
+                }
             `;
             break;
         }
         case "primary":
         {
             className = css `
+                background: ${theme.colors.primaryBackground};
+                color: ${theme.colors.primaryForeground};
+                padding: ${padding};
+                border: none;
+                border-radius: none;
+
+                &:hover {
+                    background: ${theme.colors.hoveredPrimaryBackground};
+                }
+
+                &:active {
+                    background: ${theme.colors.pressedBackground};
+                    color: ${theme.colors.pressedForeground};
+                }
+
+                &:focus {
+                    outline: 1px dashed ${theme.colors.focusDashes};
+                    outline-offset: -0.7rem;
+                }
             `;
             break;
         }
         case "danger":
         {
             className = css `
+                background: ${theme.colors.dangerBackground};
+                color: ${theme.colors.dangerForeground};
+                padding: ${padding};
+                border: none;
+                border-radius: none;
+
+                &:hover {
+                    background: ${theme.colors.hoveredDangerBackground};
+                }
+
+                &:active {
+                    background: ${theme.colors.pressedBackground};
+                    color: ${theme.colors.pressedForeground};
+                }
+
+                &:focus {
+                    outline: 1px dashed ${theme.colors.focusDashes};
+                    outline-offset: -0.7rem;
+                }
             `;
             break;
         }
-        case "white-outline":
+        case "outline":
         {
+            const dark = Color(theme.colors.background).isDark();
+            const color = dark ? "#fff" : "#000";
+            const hoverBg = dark ? "rgba(255, 255, 255, 0.4)" : "rgba(0, 0, 0, 0.4)";
+            const pressedCharColor = dark ? "#000" : "#fff";
+
             className = css `
+                background: none;
+                color: ${color};
+                padding: ${padding};
+                border: 2px solid ${color};
+                border-radius: none;
+
+                &:hover {
+                    background: ${hoverBg};
+                }
+
+                &:active {
+                    background: ${color};
+                    color: ${pressedCharColor};
+                }
+
+                &:focus {
+                    outline: 1px dashed ${color};
+                    outline-offset: -0.7rem;
+                }
             `;
             break;
         }
-        case "white-outline-primary":
+        case "outline-primary":
         {
+            const dark = Color(theme.colors.background).isDark();
+            const color = dark ? "#fff" : "#000";
+            const bg = dark ? "rgba(255, 255, 255, 0.4)" : "rgba(0, 0, 0, 0.4)";
+            const hoverBg = dark ? "rgba(255, 255, 255, 0.6)" : "rgba(0, 0, 0, 0.6)";
+            const pressedCharColor = dark ? "#000" : "#fff";
+
             className = css `
+                background: ${bg};
+                color: ${color};
+                padding: ${padding};
+                border: 2px solid ${color};
+                border-radius: none;
+
+                &:hover {
+                    background: ${hoverBg};
+                }
+
+                &:active {
+                    background: ${color};
+                    color: ${pressedCharColor};
+                }
+
+                &:focus {
+                    outline: 1px dashed ${color};
+                    outline-offset: -0.7rem;
+                }
             `;
             break;
         }
