ea1c90a885da5853c63eee18b889bac9b860e96d
hydroperfox <hydroperfox@gmail.com>
2025-02-03 08:47:37 -0300

    Update

--- demo/src/Main.tsx
+++ demo/src/Main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import { Button } from "@hydroper/metrocomponents";
+import "@fontsource/open-sans/100.css";
+import "@fontsource/open-sans/200.css";
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/500.css";
+import "@fontsource/open-sans/700.css";
+
 
 function App()
 {
