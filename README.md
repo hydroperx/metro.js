552fed4b396f7c4801d2ee2529e07540798d5ec7
hydroperfox <hydroperfox@gmail.com>
2025-02-17 12:20:55 -0300

    Update

--- README.md
+++ README.md
@@ -31,8 +31,7 @@ npm i @fontsource/open-sans
 Import it into your entry point TypeScript as follows:
 
 ```ts
-import "@fontsource/open-sans/100.css";
-import "@fontsource/open-sans/200.css";
+import "@fontsource/open-sans/300.css";
 import "@fontsource/open-sans/400.css";
 import "@fontsource/open-sans/500.css";
 import "@fontsource/open-sans/700.css";
--- demo/src/Main.tsx
+++ demo/src/Main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
-import { Button } from "@hydroper/metrocomponents";
-import "@fontsource/open-sans/100.css";
-import "@fontsource/open-sans/200.css";
+import { Button, Label, VGroup, ThemeProvider, darkTheme } from "@hydroper/metrocomponents";
+import "@fontsource/open-sans/300.css";
 import "@fontsource/open-sans/400.css";
 import "@fontsource/open-sans/500.css";
 import "@fontsource/open-sans/700.css";
@@ -9,7 +8,12 @@ import "@fontsource/open-sans/700.css";
 
 function App()
 {
-    return <div></div>;
+    return <ThemeProvider value={darkTheme}>
+        <VGroup gap={5}>
+            <Label variant="heading1">Hi there</Label>
+            <Button variant="outline-primary">Click me</Button>
+        </VGroup>
+    </ThemeProvider>;
 }
 
 const root = createRoot(document.getElementById("root")!);
