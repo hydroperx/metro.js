cbf38218c929daed0eb5691f13a159fdc7d92bc8
hydroperfox <hydroperfox@gmail.com>
2025-02-17 18:37:34 -0300

    Update

--- demo/src/Main.tsx
+++ demo/src/Main.tsx
@@ -5,7 +5,6 @@ import "@fontsource/open-sans/400.css";
 import "@fontsource/open-sans/500.css";
 import "@fontsource/open-sans/700.css";
 
-
 function App()
 {
     return <ThemeContext.Provider value={darkTheme}>
