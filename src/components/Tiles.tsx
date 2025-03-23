ff0e11947d5a6b22bc7dc3b39ca3547ccda48f70
realhydroper <hydroperfox@gmail.com>
2025-03-23 16:30:32 -0300

    Update

--- src/components/Tiles.tsx
+++ src/components/Tiles.tsx
@@ -17,6 +17,7 @@ export type { TileSize } from "com.hydroper.tilelayout";
 
 const tileClass = "Tile";
 const tileIconClass = "Tile-icon";
+const tileLabelClass = "Tile-label";
 // Tile pages are the different slide contents a tile
 // may display one at a time.
 const tilePageClass = "Tile-page";
@@ -117,6 +118,10 @@ const Div = styled.div<{
     & .Tile[data-size="small"] .${tileIconClass} {
         width: 2.5rem;
         height: 2.5rem;
+    }
+
+    & .Tile[data-size="small"] .${tileLabelClass} {
+        display: none;
     }
 
     & .${tileClass} .${tileCheckedRectClass} {
